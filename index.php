1faeb4eaaac49ec63f11aa55aa6a9c2c337a7be4
Matthieu Napoli <matthieu@mnapoli.fr>
2016-10-30 00:27:17 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+    require_once dirname(__FILE__) . '/vendor/autoload.php';
+
+test(array(
+        '$_GET["foo"] = "bar\'bar"; $next();',
+        'security_middleware(); $next();',
+        'echo "1!\n"; $next(); echo "3!\n";',
+        'echo "2!\n"; $next();',
+        'echo $_GET["foo"] . "\n"; $next();',
+    ))
+
+    ;
---
+++ theframework.php
@@ -0,0 +1,62 @@
+<?php
+
+/* -----------------------------------------------------------------------------*/
+/*                                 THEPHP6FRAMEWORK                             */
+/* -----------------------------------------------------------------------------*/
+
+// FRAMEWORK CORE
+function test(array $middlewares)
+{
+    $next = 'nothing';
+    foreach (array_reverse($middlewares) as $middleware) {
+        $next = array(new LazyNext($middleware, $next), 'run');
+    }
+    $next();
+}
+
+class LazyNext
+{
+    function __construct($middleware, $next)
+    {
+        $this->middleware = $middleware;
+        $this->next = $next;
+    }
+    function run()
+    {
+        // Put the variable in the local scope so that it's available in the middleware
+        $next = $this->next;
+        // TODO replace eval with Closures if they are ever implemented in PHP
+        eval($this->middleware);
+    }
+}
+
+// Does nothing
+function nothing() {}
+
+
+/* -----------------------------------------------------------------------------*/
+/*                              MIDDLEWARE MARKETPLACE                          */
+/* -----------------------------------------------------------------------------*/
+/*            Edit this file and add your open source middlewares!!! :)         */
+/* -----------------------------------------------------------------------------*/
+
+// Secures the application BIG TIME
+// @see http://php.net/manual/en/security.magicquotes.php
+function security_middleware()
+{
+    if (isset($_GET)) {
+        foreach ($_GET as &$value) {
+            $value = addslashes($value);
+        }
+    }
+    if (isset($_POST)) {
+        foreach ($_POST as &$value) {
+            $value = addslashes($value);
+        }
+    }
+    if (isset($_SESSION)) {
+        foreach ($_SESSION as &$value) {
+            $value = addslashes($value);
+        }
+    }
+}
